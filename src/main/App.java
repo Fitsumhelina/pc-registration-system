f6e74da8cbb47cab5391793ed64838059c0eced0
Fitsumhelina <permin1501424@gmail.com>
2025-02-02 21:24:35 +0300

    stash

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,9 @@
 package main;
 
+import main.gui.LoginFrame; // Ensure that this class exists in the specified package
+
 public class App {
-    
+    public static void main(String[] args) {
+        new LoginFrame();
+    }
 }
